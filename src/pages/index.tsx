5caec14ca52cf8a3ae0d096b4b59542ceebc5af1
ThunderBolt-OS <soham.bhoir@somaiya.edu>
2024-03-15 22:07:43 +0530

    added meta tags in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,6 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
-import { Box } from '@mui/material';
-
-import styles from './index.module.css';
+import Head from '@docusaurus/Head';
 
 // home sections
 import Banner from '@site/src/sections/home/banner';
@@ -18,17 +12,84 @@ import Insides from '@site/src/sections/home/insides';
 export default function Home(): JSX.Element {
 	const { siteConfig } = useDocusaurusContext();
 	return (
-		<Layout
-			title={`Hello from ${siteConfig.title}`}
-			description='Description will go into a meta tag in <head />'
-		>
-			{/* <Box> */}
+		<Layout title={`Hello from ${siteConfig.title}`}>
+			<Head>
+				<meta
+					name='author'
+					content='Soham Bhoir'
+				/>
+				<meta
+					name='robots'
+					content='index, follow'
+				/>
+				<meta
+					name='revisit-after'
+					content='1 day'
+				/>
+				<meta
+					name='language'
+					content='English'
+				/>
+				<meta
+					name='generator'
+					content='VS Code'
+				/>
+				<meta
+					name='distribution'
+					content='global'
+				/>
+				<meta
+					property='og:image'
+					content='https://quantummlhandbook.vercel.app/logo3.png'
+				/>
+				<meta
+					property='image'
+					content='https://quantummlhandbook.vercel.app/logo3.png'
+				/>
+				<meta
+					name='description'
+					content={siteConfig.tagline}
+				/>
+				<meta
+					name='keywords'
+					content='quantum machine learning, quantum ml, quantum machine learning tutorial, handbook, soham bhoir, quantum machine learning handbook, machine learning, quantum algorithm, quantum gates, quantum circuit, quantum superposition, quantum entanglement'
+				/>
+				<meta
+					name='twitter:title'
+					content={siteConfig.title}
+				/>
+				<meta
+					name='twitter:description'
+					content={siteConfig.tagline}
+				/>
+				<meta
+					name='twitter:image'
+					content='https://quantummlhandbook.vercel.app/logo3.png'
+				/>
+
+				<link
+					rel='canonical'
+					href='https://quantummlhandbook.vercel.app/'
+				/>
+
+				<link
+					rel='preconnect'
+					href='https://quantummlhandbook.vercel.app/logo3.png'
+				/>
+				<script type='application/ld+json'>
+					{JSON.stringify({
+						'@context': 'https://schema.org/',
+						'@type': 'Organization',
+						name: 'Meta Open Source',
+						url: 'https://opensource.fb.com/',
+						logo: 'https://opensource.fb.com/img/logos/Meta-Open-Source.svg'
+					})}
+				</script>
+			</Head>
 			<Banner />
 			<Features />
 			<Insides />
-			{/* <Testimonials /> */}
 			<CallToAction />
-			{/* </Box> */}
 		</Layout>
 	);
 }
